4549ab93b1d3d395e82866da493566c60eed254d
Alok KumarS Singh <singhalokkumar427@gmail.com>
2022-04-17 12:08:29 +0530

    Initial commit

---
+++ src/com/company/CWH_04_literals.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class CWH_04_literals {
+    public static void main(String[] args) {
+
+
+        byte age = 35;
+        double d1 = 5535.6859D;
+        float f1 = 689459.5659f;
+        long agedino = 356955L;
+        char ch = 'A';
+        short age3 = 645;
+        System.out.println(age);
+
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int num1  = 45;
+        int num2  = 65;
+        int num3  = 65;
+        int num4  = 654;
+        int num5  = 98;
+        int num6  = 25;
+        int sum = num1 + num2 + num3 + num4 + num5 + num6;
+        System.out.println(sum);
+    }
+}
